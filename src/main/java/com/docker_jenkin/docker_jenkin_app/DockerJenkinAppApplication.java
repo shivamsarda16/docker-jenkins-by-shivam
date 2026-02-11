7755c982870b35595f258d7e9e83eec45e6bffbb
C-ShivamSarda <shivam.s@accubits.com>
2026-02-11 13:28:11 +0530

    initial commit

---
+++ src/main/java/com/docker_jenkin/docker_jenkin_app/DockerJenkinAppApplication.java
@@ -0,0 +1,13 @@
+package com.docker_jenkin.docker_jenkin_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker_jenkin/docker_jenkin_app/DockerJenkinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker_jenkin.docker_jenkin_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
